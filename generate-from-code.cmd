27aaf8f63ce148c781cb1aa5cc9ce0949efccefa
dirkarnez <smalldirkalex@gmail.com>
2021-08-27 11:08:14 +0800

    - update generate-from-code.cmd

--- generate-from-code.cmd
+++ generate-from-code.cmd
@@ -1,6 +1,5 @@
 docker run --rm -it --env GOPATH=/go -v "%CD%:/go/src/dirkarnez/go-swagger-playground" -w /go/src/dirkarnez/go-swagger-playground ^
  quay.io/goswagger/swagger generate spec --scan-models -o ./swagger-ui/swagger.json
 
+REM remember to add  base <href="/swagger-ui/"> to swagger-ui/index.html
 pause
-
-
